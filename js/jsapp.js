bdc60f706b3913d87135c6a013f2990f7c133cfd
collybaby <colleengeorge111@gmail.com>
2016-02-23 19:04:36 -0800

    Initial Commit

---
+++ js/jsapp.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$("#list").append("<li>" + i + "</li>")
+var i = (i - 1, i < 100; i++)
+var x = i % 3 == 0;
+var y = i % 5 == 0;
+
+if (x); {
+	append.("<li>" + "fizz" + "</li>")
+}
+else if (y); {
+	append.("<li>" + "buzz" + "</li>")
+}
+else (i); {
+	append.("<li>" + i + "</li>")
+}
