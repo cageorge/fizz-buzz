7482859e8af9993e356bfdf63bac68c5ab57fa48
collybaby <colleengeorge111@gmail.com>
2016-02-24 20:29:15 -0800

    js update

--- js/app.js
+++ js/app.js
@@ -1,23 +1,19 @@
 $(document).ready(function() {
-	$.append("<body>" + i + "</body>")
 
-var x = i % 3 == 0;
-var y = i % 5 == 0;
-var z = i % 5 == 0, i % 3 == 0;
-var i = 1;
-
-for (i = 1, i < 100; i++){
-
-if (x); {
-	append.("<body>" + "fizz" + "</body>")
-}
-else if (y); {
-	append.("<body>" + "buzz" + "</body")
-}
-else if (z); {
-	append.("<body>" + "fizzbuzz" + "</body>")
-}
-else (i!== x,y,z); {
-	append.("<body>" + i + "</body>")
-}
-}
+	for (i = 1; i <= 100; i++) {
+		console.log(i);
+	
+		if (i % 5 == 0 && i % 3 == 0) {
+			$("#output").append("<div>" + "fizzbuzz" + "</div>")
+		}
+		else if (i % 5 == 0) {
+			$("#output").append("<div>" + "buzz" + "</div")
+		}
+		else if (i % 3 == 0) {
+			$("#output").append("<div>" + "fizz" + "</div>")
+		}
+		else {
+			$("#output").append("<div>" + i + "</div>")
+		}
+	}
+})
